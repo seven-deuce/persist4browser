ad468f12078d7f083cab9ec5d9576e1ed25d6a05
Mehdi <alialipoor1985@yahoo.com>
2021-07-16 18:44:08 +0400

    typescript defs

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+export = persist4browser;
+declare function persist4browser(options?: Partial<OptionType>): {
+    save: typeof persistState;
+    read: typeof readState;
+};
+declare function persistState(state: {}): {};
+declare function readState(initialState?: {}): any;
+declare type OptionType = {
+    fields: string[]
+    expire: string
+    prefix: string
+    encrypt: null | undefined | string | boolean
+}
---
+++ webpack.config.d.ts
@@ -0,0 +1,10 @@
+export const entry: string;
+export namespace output {
+    const path: string;
+    const filename: string;
+}
+export namespace resolve {
+    namespace fallback {
+        const crypto: boolean;
+    }
+}
